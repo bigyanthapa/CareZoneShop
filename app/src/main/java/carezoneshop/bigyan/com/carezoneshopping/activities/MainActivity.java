062c51172766db2930fb2a58e5d6fe919e4593a8
Bigyan Thapa <bigyan_4424@yahoo.com>
2015-12-08 01:42:10 -0600

    Added Expandable List

--- app/src/main/java/carezoneshop/bigyan/com/carezoneshopping/activities/MainActivity.java
+++ app/src/main/java/carezoneshop/bigyan/com/carezoneshopping/activities/MainActivity.java
@@ -1,19 +1,29 @@
 package carezoneshop.bigyan.com.carezoneshopping.activities;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ExpandableListView;
 import android.widget.ProgressBar;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import carezoneshop.bigyan.com.carezoneshopping.R;
+import carezoneshop.bigyan.com.carezoneshopping.adapter.ExpandableListAdapter;
+import carezoneshop.bigyan.com.carezoneshopping.fragments.AddItem;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -21,16 +31,26 @@ public class MainActivity extends AppCompatActivity {
     @InjectView(R.id.content_swipe_refresh)
     SwipeRefreshLayout mSwipeRefreshLayout;
 
-    @InjectView(R.id.content_recycler)
-    RecyclerView mRecyclerView;
-
     @InjectView(R.id.progress_bar)
     ProgressBar mProgressBar;
 
     @InjectView(R.id.toolbar)
     Toolbar toolbar;
 
+    @InjectView(R.id.myFAB)
+     FloatingActionButton myFab;
+
     boolean isLoading = true;
+
+    Fragment fragment = null;
+
+    ExpandableListAdapter listAdapter;
+
+    @InjectView(R.id.expandableList)
+    ExpandableListView expListView;
+
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,50 +61,84 @@ public class MainActivity extends AppCompatActivity {
 
         ButterKnife.inject(this);
 
+        hideProgress();
+
         toolbar.setTitle("");
         setSupportActionBar(toolbar);
 
+        // preparing list data
+        prepareListData();
 
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        mRecyclerView.setHasFixedSize(true);
-        //initializeAdapter();
+        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
 
-        initializeRefreshLayout();
-        initializeOnScrollForRecyclerView();
-    }
+        // setting list adapter
+        expListView.setAdapter(listAdapter);
 
-//    public void initializeAdapter() {
-//        mPostAdapter = new PostAdapter(this, new ArrayList<Post>());
-//        mRecyclerView.setAdapter(mPostAdapter);
-//    }
+        //Display Add ItemModel fragment when this floating button is clicked
+        myFab.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
 
-    public void initializeRefreshLayout() {
-        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                //mPostPresenter.loadPosts(true);
-            }
-        });
-    }
+                fragment = new AddItem();
+                if (fragment != null) {
+                    FragmentManager fm = getFragmentManager();
+                    FragmentTransaction fragmentTransaction = fm.beginTransaction();
+                    fragmentTransaction.setCustomAnimations(android.R.animator.fade_in,
+                            android.R.animator.fade_out);
 
-    public void initializeOnScrollForRecyclerView() {
-        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
-            @Override
-            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                    fragmentTransaction.replace(R.id.content_swipe_refresh, fragment);
 
-                int visibleItemCount = recyclerView.getLayoutManager().getChildCount();
-                int totalItemCount = recyclerView.getLayoutManager().getItemCount();
-                int pastVisiblesItems = ((LinearLayoutManager) recyclerView.getLayoutManager()).findFirstVisibleItemPosition();
-
-                if (!isLoading) {
-                    if ((visibleItemCount + pastVisiblesItems) >= totalItemCount) {
-                        isLoading = true;
-                        //mPostPresenter.loadPosts(false);
-                    }
+                    fragmentTransaction.commit();
+                } else {
+                    // error in creating fragment
+                    Log.e("MainActivity", "Error in creating fragment");
                 }
             }
         });
     }
+
+    /*
+     * Preparing the list data
+     */
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+
+        // Adding child data
+        listDataHeader.add("Top 250");
+        listDataHeader.add("Now Showing");
+        listDataHeader.add("Coming Soon..");
+
+        // Adding child data
+        List<String> top250 = new ArrayList<String>();
+        top250.add("The Shawshank Redemption");
+        top250.add("The Godfather");
+        top250.add("The Godfather: Part II");
+        top250.add("Pulp Fiction");
+        top250.add("The Good, the Bad and the Ugly");
+        top250.add("The Dark Knight");
+        top250.add("12 Angry Men");
+
+        List<String> nowShowing = new ArrayList<String>();
+        nowShowing.add("The Conjuring");
+        nowShowing.add("Despicable Me 2");
+        nowShowing.add("Turbo");
+        nowShowing.add("Grown Ups 2");
+        nowShowing.add("Red 2");
+        nowShowing.add("The Wolverine");
+
+        List<String> comingSoon = new ArrayList<String>();
+        comingSoon.add("2 Guns");
+        comingSoon.add("The Smurfs 2");
+        comingSoon.add("The Spectacular Now");
+        comingSoon.add("The Canyons");
+        comingSoon.add("Europa Report");
+
+        listDataChild.put(listDataHeader.get(0), top250); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), nowShowing);
+        listDataChild.put(listDataHeader.get(2), comingSoon);
+    }
+
+
     public void showProgress() {
         mProgressBar.setVisibility(View.VISIBLE);
         mSwipeRefreshLayout.setVisibility(View.INVISIBLE);
---
+++ app/src/main/java/carezoneshop/bigyan/com/carezoneshopping/model/ItemModel.java
@@ -0,0 +1,28 @@
+package carezoneshop.bigyan.com.carezoneshopping.model;
+
+/**
+ * Created by bigyanthapa on 12/7/15.
+ */
+public class ItemModel {
+
+    private String itemName;
+    private String itemCategory;
+
+    public ItemModel(){}
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getItemCategory() {
+        return itemCategory;
+    }
+
+    public void setItemCategory(String itemCategory) {
+        this.itemCategory = itemCategory;
+    }
+}
